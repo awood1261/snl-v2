539f11da220255fe29ee5bbebd0119c2723967d8
Alex Wood <alex.c.wood@gmail.com>
2023-03-05 17:29:07 -0500

    added storybook to the project

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react').Preview } */
+import 'tailwindcss/tailwind.css';
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: "light",
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
