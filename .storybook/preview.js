b73cdd77ab5c2f25676a703aa8a0f91c0ce66f8d
Alex Wood <alex.c.wood@gmail.com>
2023-03-09 05:35:22 -0500

    add bg add for sb

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,24 @@
 /** @type { import('@storybook/react').Preview } */
 import 'tailwindcss/tailwind.css';
+
 const preview = {
   parameters: {
     backgrounds: {
-      default: "light",
+      default: "darkbrown",
+      values: [
+        {
+          name: "darkbrown",
+          value: "#150d0c"
+        },
+        {
+          name: "coffeebrown",
+          value: "#5e2408"
+        },
+        {
+          name: "rust",
+          value: "#693d1a"
+        }
+      ]
     },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
